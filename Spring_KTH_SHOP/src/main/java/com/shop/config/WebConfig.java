83f4482fe29e41d2aa956bb576a0affc71dd53eb
Kim_Tae_Hun <53397420+BaecMari@users.noreply.github.com>
2021-02-24 18:00:01 +0900

    JavaConfig 형식으로 Hello world! 까지 띄움및 OneNote 정리

---
+++ Spring_KTH_SHOP/src/main/java/com/shop/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.shop.config;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        //return new Class[0];
+        //아래 내용은 RootConfig.java 작성 후
+        return new Class[] {RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        //return new Class[0];
+        //아래 내용은 ServletConfig.java 작성 후
+        //return null;
+        return new Class[] {ServletConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        //return new String[0];
+        //return null;
+        return new String[] {"/"};
+    }
+}
